1ce96a4d9c3eaed31cf19c01b2861d824881a570
hefeixiong <1074535830@qq.com>
2022-06-15 16:48:12 +0800

    contorller/article.js

---
+++ app/controller/article.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Contorller = require('egg').Controller;
+const moment = require('moment');
+
+class ArticleContorller extends Contorller {
+  async create() {
+    const { ctx } = this;
+    const createData = {
+      ...ctx.request.body,
+      createTime: moment().format('YYYY-MM-DD HH:mm:ss'),
+    };
+    const res = await ctx.service.article.create(createData);
+    if (res) {
+      ctx.body = {
+        status: 200,
+        data: res,
+      };
+    } else {
+      ctx.body = {
+        status: 500,
+        errMsg: '发布文章失败',
+      };
+    }
+  }
+
+  async lists() {
+    const { ctx } = this;
+    const resData = await ctx.service.article.lists();
+    if (resData) {
+      ctx.body = {
+        status: 200,
+        data: resData,
+      };
+    } else {
+      ctx.body = {
+        status: 500,
+        errMsg: '查询文章列表失败',
+      };
+    }
+  }
+
+  async datail() {
+    const { ctx } = this;
+    const resData = await ctx.service.article.detail(ctx.query.id);
+    if (resData) {
+      ctx.body = {
+        status: 200,
+        data: resData,
+      };
+    } else {
+      ctx.body = {
+        status: 500,
+        errMsg: '查询文章详情失败',
+      };
+    }
+  }
+
+  async delete() {
+    const { ctx } = this;
+    const res = await ctx.service.article.delete(ctx.query.id);
+    if (res) {
+      ctx.body = {
+        status: 200,
+        data: res,
+      };
+    } else {
+      ctx.body = {
+        status: 500,
+        errMsg: '查询文章列表失败',
+      };
+    }
+  }
+}
+
+module.exports = ArticleContorller;
