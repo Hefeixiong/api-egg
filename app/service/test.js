ea9bff1f5f4803d3211f2d2a5cf524237fbdce08
hefeixiong <1074535830@qq.com>
2022-06-15 21:01:25 +0800

    优化代码

--- app/service/test.js
+++ app/service/test.js
@@ -6,14 +6,12 @@ class Test extends Service {
   async index() {
     // 连接数据库
     const { app } = this;
-    const data = await app.mysql.select('article');
-    // const data = await app.mysql.query('select * from article');
-    console.log(data);
-    // return {
-    //   id: '123',
-    //   name: '测试',
-    // };
-    return data;
+    // const data = await app.mysql.select('article');
+    // 获取的是一个数组
+    const data = await app.mysql.query('select * from article');
+    const string = JSON.stringify(data);
+    const dataObject = JSON.parse(string);
+    return dataObject[0];
   }
 }
 
